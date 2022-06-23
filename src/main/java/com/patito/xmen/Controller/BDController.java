c4565c6ea013d5a6c7dc94dc3494b4cf73e69819
Gaspar <e972136@gmail.com>
2022-06-23 10:50:44 -0600

    Integracion controllador para BD con el fin de hacer pruebas

--- src/main/java/com/patito/xmen/Controller/BDController.java
+++ src/main/java/com/patito/xmen/Controller/BDController.java
@@ -4,6 +4,7 @@ import com.patito.xmen.entity.Candidato;
 import com.patito.xmen.repository.CandidatoRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +26,7 @@ public class BDController {
         this.repository = repository;
     }
 
-    @GetMapping("/limpiar")
+    @DeleteMapping("/limpiar")
     @Operation(summary = "limpiar BD")
     public ResponseEntity<String> limpiar() throws Exception {
         try{
