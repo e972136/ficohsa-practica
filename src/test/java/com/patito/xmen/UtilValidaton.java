25b54144b3f4c06fb8bee8b4a78b765b24855d63
Gaspar <e972136@gmail.com>
2022-07-20 10:58:07 -0600

    DnaValidatorTest

---
+++ src/test/java/com/patito/xmen/UtilValidaton.java
@@ -0,0 +1,82 @@
+package com.patito.xmen;
+
+import com.patito.xmen.dao.CandidatoRequest;
+import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import javax.validation.ConstraintViolation;
+
+import javax.validation.Validation;
+import java.util.Set;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class UtilValidaton {
+
+
+    @Test
+    public void utilCandidatoRequestTest01(){
+        Validator validator;
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+        String[]base = {
+            "XTGCXA",
+                    "CAGTGC",
+                    "TTGT",
+                    "AGAAXG",
+                    "TCACTG"};
+
+        CandidatoRequest candidatoRequest = CandidatoRequest.of(base);
+        Set<ConstraintViolation<CandidatoRequest>> validate = null;
+        validate=validator.validate(candidatoRequest);
+        assertFalse(validate.isEmpty());
+    }
+
+    @Test
+    public void utilCandidatoRequestTest02(){
+        Validator validator;
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+        String[]base = {
+            "XTGCXA",
+                    "CAGTGC",
+                    "TTGT",
+                    "AGAAXG",
+                    "TCACTG"};
+
+        CandidatoRequest candidatoRequest = CandidatoRequest.of(null);
+        Set<ConstraintViolation<CandidatoRequest>> validate = null;
+        validate=validator.validate(candidatoRequest);
+        assertFalse(validate.isEmpty());
+    }
+
+    @Test
+    public void utilCandidatoRequestTest03(){
+        Validator validator;
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+        String[]base = {
+                "XTGCXA",
+                "TCACTG"};
+
+        CandidatoRequest candidatoRequest = CandidatoRequest.of(base);
+        Set<ConstraintViolation<CandidatoRequest>> validate = null;
+        validate=validator.validate(candidatoRequest);
+        assertFalse(validate.isEmpty());
+    }
+
+    @Test
+    public void utilCandidatoRequestTest04(){
+        Validator validator;
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+        String[]base = {"ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"};
+
+        CandidatoRequest candidatoRequest = CandidatoRequest.of(base);
+        Set<ConstraintViolation<CandidatoRequest>> validate = null;
+        validate=validator.validate(candidatoRequest);
+        assertTrue(validate.isEmpty());
+    }
+}
+
--- src/test/java/com/patito/xmen/XmenApplicationTests.java
+++ src/test/java/com/patito/xmen/XmenApplicationTests.java
@@ -1,13 +1,20 @@
 package com.patito.xmen;
 
+import com.patito.xmen.Controller.BDController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class XmenApplicationTests {
 
+	@Autowired
+	private BDController bdController;
+
 	@Test
 	void contextLoads() {
+		assertThat(bdController).isNotNull();
 	}
 
 }
